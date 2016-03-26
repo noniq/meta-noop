f0a8f53c4532378fccca92c3d3903ac0a46cba06
Stefan Daschek <stefan@daschek.net>
2016-03-26 17:16:34 +0100

    Add commment.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const noops = [
   require("async.util.noop"),
   require("basic-functions").noop,
   require("blank-space").noop,
-  require("co-noop"),
+  require("co-noop"), // returns a generator function version of noop …
   require("empty").func,
   require("fj-noop").FUNC,
   require("lodash._noop"),
